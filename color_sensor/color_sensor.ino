36d34082494d29a99067a408ea7d502a4fa66a2e
rac-jac <53905495+rac-jac@users.noreply.github.com>
2020-10-21 11:11:01 +1100

    Update 2: Edited if conditions in readColor()

--- color_sensor/color_sensor.ino
+++ color_sensor/color_sensor.ino
@@ -1,10 +1,12 @@
+
+//Defining pins
 #define COLOUROUT A5
 #define COLOURS2 A4
 #define COLOURS3 A3
 
+//Colour variables 
 int frequency = 0;
-int color;
-
+int color = 0;
 
 void setup() {
  pinMode(COLOUROUT,INPUT);
@@ -13,56 +15,14 @@ void setup() {
  Serial.begin(9600);
 }
 
-void loop() {
-  /*
-  //Calibration Code
-  // Setting red filtered photodiodes to be read
-  digitalWrite(COLOURS2, LOW);
-  digitalWrite(COLOURS3, LOW);
-  // Reading the output frequency
-  frequency = pulseIn(COLOUROUT, LOW);
-  int R = frequency;
-  // Printing the value on the serial monitor
-  Serial.print("R= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.print("  ");
-  delay(100);
-
-    // Setting Green filtered photodiodes to be read
-  digitalWrite(COLOURS2, HIGH);
-  digitalWrite(COLOURS3, HIGH);
-  // Reading the output frequency
-  frequency = pulseIn(COLOUROUT, LOW);
-  int G = frequency;
-  // Printing the value on the serial monitor
-  Serial.print("G= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.print("  ");
-  delay(100);
-  
-  // Setting Blue filtered photodiodes to be read
-  digitalWrite(COLOURS2, LOW);
-  digitalWrite(COLOURS3, HIGH);
-  // Reading the output frequency
-  frequency = pulseIn(COLOUROUT, LOW);
-  int B = frequency;
-  // Printing the value on the serial monitor
-  Serial.print("B= ");//printing name
-  Serial.print(frequency);//printing RED color frequency
-  Serial.println("  ");
-  delay(100);
-*/
-  
-  
+void loop() {  
   color = readColor();
-  delay(10);
-  
+  delay(20);
   Serial.print(" Color=");
   Serial.print(color);
   Serial.print(" ");
-  delay(50);
+  delay(20);
 }
-
 
 int readColor() {
   // Setting red filtered photodiodes to be read
@@ -75,7 +35,7 @@ int readColor() {
   Serial.print("R= ");//printing name
   Serial.print(frequency);//printing RED color frequency
   Serial.print("  ");
-  delay(100);
+  delay(20);
 
     // Setting Green filtered photodiodes to be read
   digitalWrite(COLOURS2, HIGH);
@@ -87,7 +47,7 @@ int readColor() {
   Serial.print("G= ");//printing name
   Serial.print(frequency);//printing RED color frequency
   Serial.print("  ");
-  delay(100);
+  delay(20);
   
   // Setting Blue filtered photodiodes to be read
   digitalWrite(COLOURS2, LOW);
@@ -99,24 +59,29 @@ int readColor() {
   Serial.print("B= ");//printing name
   Serial.print(frequency);//printing RED color frequency
   Serial.println("  ");
-  delay(100);
+  delay(20);
 
-  //still need to play around with values
-  if(R<19 & R>6 & G<18 & G>6) {
+  //need to play around with values
+  if(R>3 & R<10 & G>18 & G<30 & B>10 & B<29) {
     color = 1; // Red
   }
-  if(G<56 & G>9 & B<58 & B>17){
-    color = 2; // Green
-  }
-  if(R<95 & R>35 & G == 35 & B<75 & B>22){
-    color = 3; // Blue
-  }
-  if(R<11 & R>4 & G<12 & G>6 & B<20 & B>12){
-    color = 4; // Yellow
-  }
-    color = 0; 
 
-  return color;  
+  else if(R>17 & R<31 & G>17 & G<28 & B>10 & B<18){
+    color = 2;  //Blue
+  }
+
+  else {
+  color = 0; 
+  }
   
+  return color;  
 }
 
+/*void start (int colourInput){
+  if(colourInput == 1){ 
+    //Run x program
+  } else{
+    if
+  }
+}
+*/
