60acfe2ed1a32bb1b8d8c6112d6c6dc2400b333f
VAN\HOME <van.vu@students.mq.edu.au>
2020-10-27 23:11:39 +1100

    Comments

--- src/Monorail-Control/MetroDoor_Adaptor.cpp
+++ src/Monorail-Control/MetroDoor_Adaptor.cpp
@@ -4,15 +4,15 @@
 
 #define DOOR_PIN 12
 
-//MetroDoor my_door(DOOR_PIN);
-
-
 MetroDoor my_door(DOOR_PIN);
 
 void door_init() {
 }
 
 bool close_door(SM_door *door_state){
+  ///// for testing without hardware
+  return true;
+  
   bool success = false;
   success = my_door.close();
   return success;
@@ -20,6 +20,9 @@ bool close_door(SM_door *door_state){
 
 
 bool open_door(SM_door *door_state){
+  ///// for testing without hardware
+  return true;
+  
   bool success = false;
   success = my_door.open();
   return success;
