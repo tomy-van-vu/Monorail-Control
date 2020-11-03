019346f4813c69d2d86a150a1a9d130b19727e99
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2020-11-03 19:52:39 +1100

    Indent code for readability

--- src/Comms.cpp
+++ src/Comms.cpp
@@ -6,12 +6,16 @@
 // Written by T4C2
 message read_msg(){
   message blue_in = NONE;
-  if (Serial3.available() > 0) blue_in = (message)Serial3.read();
+  if (Serial3.available() > 0) {
+    blue_in = (message)Serial3.read();
+  }
   return blue_in;
 }
 
 message testing_mode_read_message(){
   message blue_in = NONE;
-  if (Serial.available()) {blue_in = (message)Serial.read();}
+  if (Serial.available()) {
+    blue_in = (message)Serial.read();
+  }
   return blue_in;
 }
