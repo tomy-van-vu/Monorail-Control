e44d7664e800561b2c6206e1fbed338d64626176
van-vu-mq <36938569+van-vu-mq@users.noreply.github.com>
2020-11-04 15:23:57 +1100

    Comment out serialprints

--- src/Helpers.h
+++ src/Helpers.h
@@ -6,18 +6,18 @@
 
 #pragma once
 // FUNCTIONS TO SEND STATE DATA
-void send_stop() { quick_transmit(&bt_i, STOP); Serial.println("-- Message sent -- STOP");}
+void send_stop() { quick_transmit(&bt_i, STOP); }//Serial.println("-- Message sent -- STOP");}
 
-void send_go() { quick_transmit(&bt_i, START); Serial.println("-- Message sent -- START");}
+void send_go() { quick_transmit(&bt_i, START); }//Serial.println("-- Message sent -- START");}
 
-void send_east() { quick_transmit(&bt_i, EAST); Serial.println("-- Message sent -- EAST");}
+void send_east() { quick_transmit(&bt_i, EAST); }//Serial.println("-- Message sent -- EAST");}
 
-void send_west() { quick_transmit(&bt_i, WEST); Serial.println("-- Message sent -- WEST");}
+void send_west() { quick_transmit(&bt_i, WEST); }//Serial.println("-- Message sent -- WEST");}
 
-void send_emergency() { quick_transmit(&bt_i, EMERGENCY); Serial.println("-- Message sent -- EMERGENCY");}
+void send_emergency() { quick_transmit(&bt_i, EMERGENCY); }//Serial.println("-- Message sent -- EMERGENCY");}
 
-void send_not_emergency() { quick_transmit(&bt_i, NONE); Serial.println("-- Message sent -- NONE");}
+void send_not_emergency() { quick_transmit(&bt_i, NONE); }//Serial.println("-- Message sent -- NONE");}
 
-void send_doors_open() { quick_transmit(&bt_i, OPEN); Serial.println("-- Message sent -- OPEN");}
+void send_doors_open() { quick_transmit(&bt_i, OPEN); }//Serial.println("-- Message sent -- OPEN");}
 
-void send_doors_closed() { quick_transmit(&bt_i, CLOSE); Serial.println("-- Message sent -- CLOSE");}
+void send_doors_closed() { quick_transmit(&bt_i, CLOSE); }//Serial.println("-- Message sent -- CLOSE");}
